1e5af605f539fbfaf59698d4e6d29f080a6b9c2a
VarunS1997 <varun.1997.singh@gmail.com>
2016-04-16 19:36:39 -0700

    Added Repo Link

--- modules/footer.php
+++ modules/footer.php
@@ -1,4 +1,5 @@
 <div class="wrapper" id="footer-wrapper">
-    &copy; 2016 Gunn Robotics Team. All Rights Reserved.
+    &copy; 2016 Gunn Robotics Team. All Rights Reserved.<br />
+    Interested in our website? Learn about it <a href="https://github.com/VarunS1997/grtWebsite">here!</a>
     <!-- Eventual Validation Information to be included here -->
 </div>
