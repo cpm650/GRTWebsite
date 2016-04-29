8ccec245f7bca11a1b94697e5b40819e68eb0ea8
Varun Singh <varun.1997.singh@gmail.com>
2016-04-29 15:35:03 -0700

    Background image fade

---
+++ JS/homeScroll.js
@@ -0,0 +1,50 @@
+// GLOBALS
+var timer;
+var timeGap = 5 * 1000; //user defined
+var frameSteps = 100; //user defined
+var frameTimeGap = 10; //user defined
+
+var timeToContinue;
+
+var imageSources = ["imageAssets/home-image.jpg", "imageAssets/stock.jpg"];
+var imageIndex = 0;
+
+var topImage;
+var bottomImage;
+
+function initImgScroll(){
+    topImage = document.getElementById("topImage");
+    bottomImage = document.getElementById("bottomImage");
+
+    topImage.style.opacity = 1;
+
+    timeToContinue = new Date().getTime() + timeGap;
+
+    timer = setInterval(function(){
+        if(timeToContinue < new Date().getTime()){
+            imageProgress();
+        }
+    }, frameTimeGap);
+}
+
+function imageProgress(){
+    if(topImage.style.opacity <= 1/frameSteps){
+        changeImage();
+    } else {
+        topImage.style.opacity = topImage.style.opacity - 1/frameSteps;
+    }
+}
+
+function changeImage(){
+    imageIndex++;
+
+    if(imageIndex >= imageSources.length){
+        imageIndex = 0;
+    }
+
+    topImage.style.backgroundImage = "url('" + imageSources[imageIndex] + "')";
+    topImage.style.opacity = 1;
+    bottomImage.style.backgroundImage = "url('" + imageSources[imageIndex + 1 >= imageSources.length ? 0 : imageIndex + 1] + "')";
+
+    timeToContinue = new Date().getTime() + timeGap;
+}
--- index.php
+++ index.php
@@ -8,6 +8,8 @@
     <title>GRT | Home</title>
     <link rel="stylesheet" href="CSS/primaryStyle.css" type="text/css" />
 
+    <script type="text/javascript" src="JS/homeScroll.js"></script>
+
     <!--HOMEPAGE SPECIFIC CSS TO IMPROVE PERFORMANCE-->
     <!-- TO BE MOVED EXTERNALLY -->
     <style type="text/css">
@@ -21,19 +23,29 @@
         color: rgba(255, 255, 255, 1);
     }
 
-    div#homeBody{
+    div.bgImage{
         height: 100%; /*Change this to height of background when applicable*/
-        background-image: url('imageAssets/home-image.jpg');
         background-size: 100%;
         overflow: hidden;
+    }
+
+    div#topImage{
+        background-image: url('imageAssets/home-image.jpg');
+    }
+
+    div#bottomImage{
+        background-image: url('imageAssets/stock.jpg');
     }
 
     ul#navList li a:hover {
         background-color: rgba(0, 0, 0, .5); /*Default*/
     }
 
-    div#imageFilter{
+    div#bodyContainer{
+        position: absolute;
+        top: -.1px;
         height: 100%;
+        background-size: 100%;
         background-color: rgba(0, 0, 0, 0.25);
     }
 
@@ -80,39 +92,41 @@
     }
     </style>
 </head>
-<body>
-    <div class="wrapper" id="homeBody">
-        <div class="wrapper" id="homeNav">
-            <div id="GRTEmblem"><a href="index.php"><img src="imageAssets/logo.svg" alt="GRT Logo"></a></div>
-            <div id="navContainer">
-                <ul id="navList">
-                    <li><a href="about.php">About</a></li>
-                    <li><a href="first.php">First</a></li>
-                    <li><a href="media.php">Media</a></li>
-                    <li><a href="resources.php">Resources</a></li>
-                    <li><a href="sponsors.php">Sponsors</a></li>
-                    <li><a href="joining.php">Joining</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                </ul>
-            </div>
+<body onload="initImgScroll()">
+    <div class="bgImage wrapper" id="bottomImage">
+        <div class="bgImage wrapper" id="topImage">
         </div>
-        <div class="wrapper" id="imageFilter">
-            <div class="wrapper" id="homeFocus">
-                <span id="homeHeader">GRT 192</span><br />
-                We're a robotics team from Gunn High School (Palo Alto, CA) that competes in the FIRST Robotics Competitions as Team 192.
-                <a id="focusButton" href="">See what we do</a>
-            </div>
-            <div class="wrapper social-media" id="footer">
-                <a href="https://www.facebook.com/GRT192">
-                    <img alt="Like us on Facebook" src="imageAssets/FB-f-Logo__white_50.png"/>
-                </a>
-                <a href="https://twitter.com/GRT192">
-                    <img alt="Follow us on Twitter" src="imageAssets/TwitterLogo_white.png"/>
-                </a>
-                <a href="https://www.youtube.com/user/gunnrobotics192">
-                    <img alt="Subscribe to us on Youtube" src="imageAssets/YouTube_light_color_icon.png"/>
-                </a>
-            </div>
+    </div>
+    <div class="wrapper" id="homeNav">
+        <div id="GRTEmblem"><a href="index.php"><img src="imageAssets/logo.svg" alt="GRT Logo"></a></div>
+        <div id="navContainer">
+            <ul id="navList">
+                <li><a href="about.php">About</a></li>
+                <li><a href="first.php">First</a></li>
+                <li><a href="media.php">Media</a></li>
+                <li><a href="resources.php">Resources</a></li>
+                <li><a href="sponsors.php">Sponsors</a></li>
+                <li><a href="joining.php">Joining</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="wrapper" id="bodyContainer">
+        <div class="wrapper" id="homeFocus">
+            <span id="homeHeader">GRT 192</span><br />
+            We're a robotics team from Gunn High School (Palo Alto, CA) that competes in the FIRST Robotics Competitions as Team 192.
+            <a id="focusButton" href="">See what we do</a>
+        </div>
+        <div class="wrapper social-media" id="footer">
+            <a href="https://www.facebook.com/GRT192">
+                <img alt="Like us on Facebook" src="imageAssets/FB-f-Logo__white_50.png"/>
+            </a>
+            <a href="https://twitter.com/GRT192">
+                <img alt="Follow us on Twitter" src="imageAssets/TwitterLogo_white.png"/>
+            </a>
+            <a href="https://www.youtube.com/user/gunnrobotics192">
+                <img alt="Subscribe to us on Youtube" src="imageAssets/YouTube_light_color_icon.png"/>
+            </a>
         </div>
     </div>
 </body>
