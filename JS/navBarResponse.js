4f5d83ef8ff549608ed51f7164f4b7dce01d6ce4
Varun Singh <varun.1997.singh@gmail.com>
2016-05-10 13:32:10 -0700

    fixed pointers for JS

--- JS/navBarResponse.js
+++ JS/navBarResponse.js
@@ -1,10 +1,13 @@
 var currentSubNav;
+var originalNav;
 var subNavList;
 var pointersList;
 
 //Functions
 function navigationInit(n){
     currentSubNav = n;
+    originalNav = n;
+
     subNavList = document.getElementsByClassName("subNavbar");
 
     pointersList = document.getElementsByClassName("pointer");
@@ -12,15 +15,17 @@ function navigationInit(n){
 }
 
 function changeSubNav(n){
-    movePrimaryPointer(n);
-
     if(subNavList[currentSubNav] != null){
         subNavList[currentSubNav].className = "wrapper subNavbar";
     }
 
     if(subNavList[n] != null){
+        movePrimaryPointer(n);
         subNavList[n].className = "active wrapper subNavbar";
+    } else if(n == originalNav){
+        movePrimaryPointer(n);
     }
+
     currentSubNav = n;
 }
 
