773cfdf3397cc7a944138af0722d9f85bc3377e6
sjiang647 <sjiang647@gmail.com>
2017-05-23 15:11:51 -0700

    test navbar2

--- team_groups.php
+++ team_groups.php
@@ -1,7 +1,7 @@
 <?php
 include "PHP_Engines/grtXmlEngine.php";
 
-$GLOBALS['pageSource'] = "team_groups"; //name of source page, no extensions. Saves a function call in engine
+$GLOBALS['pageSource'] = "Team Groups"; //name of source page, no extensions. Saves a function call in engine
 
 $pageContent = new PageContent();
 $navBar = new navBar();
