a57e570b860ce75340f03bcde0e1b733566424c0
sjiang647 <sjiang647@gmail.com>
2017-07-02 08:52:38 -0700

    test construction

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
 <!-- This is the file for what is shown on the home page -->
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<h1>
+    Website is currently under construction!
+</h1>
+<!-- <head>
     <meta charset="utf-8" />
     <title>GRT | Home</title>
 
@@ -13,7 +16,7 @@
 
     <!--HOMEPAGE SPECIFIC CSS TO IMPROVE PERFORMANCE-->
     <!-- TO BE MOVED EXTERNALLY -->
-    <style type="text/css">
+    <!-- <style type="text/css">
     div#homeNav{
         position: absolute;
         top: 0px;
@@ -148,5 +151,5 @@
             </a>
         </div>
     </div>
-</body>
+</body> -->
 </html>
