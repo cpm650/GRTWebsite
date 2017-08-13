9d31123d9687f23d6572f809bd0316898d86738d
sjiang647 <sjiang647@gmail.com>
2017-08-13 14:39:07 -0700

    change index navbar

--- modules/footer.php
+++ modules/footer.php
@@ -1,5 +1,5 @@
 <div class="wrapper" id="footer-wrapper">
-    &copy; 2016 Gunn Robotics Team. All Rights Reserved.<br />
+    &copy; 2017 Gunn Robotics Team. All Rights Reserved.<br />
     Interested in our website? Learn more about it <a href="https://github.com/VarunS1997/grtWebsite">here!</a>
     <!-- Eventual Validation Information to be included here -->
 </div>
