7421e42dec2e17155473eb9ffa49d7f05d01fbc4
Victor-Zhao-1046 <zhaotianyue@outlook.com>
2018-10-19 12:41:17 -0700

    Added WIP login page, fixed several issues

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>GRT | Log in</title>
+    <link rel="stylesheet" href="CSS/login.css" type="text/css"/>
+</head>
+<body>
+<div id="login_background">
+<div id="login_window">
+<div id="title">
+<h2>\\\\\\Admin//////</h2>
+</div>
+<div id="login_input">
+<form id="login" method="post" accept-charset='UTF-8'>
+Username:<input type="text" name="username" placeholder="Username"/><br>
+Password:<input type="text" name="password" placeholder="Password"/>
+</form>
+</div>
+<div id="login_button">
+<button id="login">Log in</button>
+<!--<a id="Login">Log in</a>-->
+</div>
+</div>
+</div>
+</body>
