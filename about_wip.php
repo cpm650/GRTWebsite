c130a811bfedcb7640fe7ea3623a831880d33d42
Victor-Zhao-1046 <zhaotianyue@outlook.com>
2018-10-19 21:55:34 -0700

    Modified new about page

--- about_wip.php
+++ about_wip.php
@@ -27,11 +27,19 @@ $navBar = new navBar();
         height:100%;
         width:50%;
         float:left;
+        font-size:14px;
+    }
+    .split-section-left p{
+        margin: 20px 30px 30px 0px;
+    }
+    .split-section-right p{
+        margin: 20px 0px 20px 30px;
     }
     .split-section-right{
         height:100%;
         width:50%;
         float:right;
+        font-size:14px;
     }
     </style>
 
@@ -47,12 +55,11 @@ $navBar = new navBar();
     <div class="sectionBody">
      
     <div class="split-section-left">
-    <p>
-    Test
-    </p>
+    <p>The Gunn Robotics Team (GRT) is a student-managed team that competes in the FIRST Robotics competition. We CAD our own designs, write our own software, and wire our own robots; we manage our sponsorships, organize and staff outreach events. Being on the Gunn Robotics Team gives every student the opportunity to make real decisions, learn from the results, and produce something amazing in the process.</p>
+    <p>All GRT students learn how to CAD and work in the shop, and develop additional expertise working in small groups focused on skills such as programming, welding, gearbox design, pneumatics, and more. We stay connected with our community, building projects for local schools and community events. We have been fortunate to work with generous sponsors, who assist us with resources and training.</p>
     </div>
     <div class="split-section-right">
-    <img src="imageAssets/team/AboutCollaboration.jpg" alt="Working as a team">
+    <img src="imageAssets/subgroups/aboutLead.jpg" alt="Working as a team">
     </div>
     
 <!--            <p>The Gunn Robotics Team (GRT) is a student-managed team that designs, builds and enters a robot in several FIRST Robotics competitions each year.  A major part of our team identity is our student ownership.  Every aspect of the team that can be executed or managed by a student, is:  leadership, organization and planning, gearbox and mechanism design; prototyping and computer simulations; fabrication using milling machines, lathes, CNC, welding and more; coding and animation; fund-raising; outreach.  Our students do it all.  We CAD our own designs, write our own software, and wire our own robots; we manage our sponsorships, organize and staff outreach events.  Being on the Gunn Robotics Team gives every student the opportunity to make real decisions, learn from the results, and produce something amazing in the process.</p>-->
