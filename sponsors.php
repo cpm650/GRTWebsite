86df8c4dec34b65d92718456481fd73b9dd946a3
Victor-Zhao-1046 <zhaotianyue@outlook.com>
2018-12-11 18:00:51 -0800

    Fixed navbar and changed certain subgroups

--- sponsors.php
+++ sponsors.php
@@ -1,35 +1,3 @@
 <?php
-include "PHP_Engines/grtXmlEngine.php";
-
-$GLOBALS['pageSource'] = "sponsors"; //name of source page, no extensions. Saves a function call in engine
-
-$pageContent = new PageContent();
-$navBar = new navBar();
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <title>GRT | FIRST</title>
-
-    <?php include "modules/std-config.php";?>
-</head>
-<body onload="navigationInit(<?php echo $navBar->currentIndex; ?>)">
-<?php
-include "modules/navBar.php";
-?>
-<div class="wrapper" id="content-wrapper">
-    <?php
-    $pageContent->generateContent();
-    ?>
-</div>
-<?php
-include "modules/footer.php";
-?>
-</body>
-</html>
-<?php
-//CLEANUP
-unset($content);
-unset($navBar);
+header('Location: index.html#sponsors_home')
 ?>
