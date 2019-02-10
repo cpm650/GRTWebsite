a2453f323c6314db5f545a0418f3764ff15592db
Victor-Zhao-1046 <zhaotianyue@outlook.com>
2019-02-09 16:30:23 -0800

    More progress on dashboard

--- dashboard.js
+++
@@ -1,8 +0,0 @@
-import 'keen-dataviz'
-console.log('Hi!');
-const chart = new KeenDataviz({
-    container: '#pie-grade', // querySelector
-    title: 'pie-grade',
-    type: 'area'
-});
-chart.render({result:[1,2,3,4,5]});
