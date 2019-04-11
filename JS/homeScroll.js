4b4e60bdebe6e578b7fcf173bfaf4703ecb610bf
Victor-Zhao-1046 <zhaotianyue@outlook.com>
2019-04-11 15:19:37 -0700

    Changed home scroll images

--- JS/homeScroll.js
+++ JS/homeScroll.js
@@ -7,7 +7,7 @@ var frameTimeGap = 5; //user defined
 
 var timeToContinue;
 
-var imageSources = ["2013-slide.jpg", "1.png", "2.png", "3.png", "4.png", "5.png", "6.png"];
+var imageSources = ["IMGP7082_crop.JPG", "2013-slide.jpg", "IMGP7004_crop.JPG", "1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "IMGP6968.JPG"];
 var imageIndex = 0;
 
 var topImage;
