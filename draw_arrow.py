a839fc0d2240e89eb71d2943c4b52a147a5839e9
Victor-Zhao-1046 <zhaotianyue@outlook.com>
2019-04-12 15:22:39 -0700

    Added arrow graphic

---
+++ draw_arrow.py
@@ -0,0 +1,13 @@
+from PIL import Image,ImageDraw
+
+depth=40
+sep=30
+n=100
+m=75
+arrow=Image.new('RGBA',(n,m),color='rgba(0,0,0,0)')
+arrow_draw=ImageDraw.Draw(arrow)
+arrow_draw.line((0,0,n/2,depth),width=5,fill='rgba(255,255,255,255)')
+arrow_draw.line((n/2-1,depth,n-1,0),width=5,fill='rgba(255,255,255,255)')
+arrow_draw.line((0,sep,n/2,depth+sep),width=5,fill='rgba(255,255,255,255)')
+arrow_draw.line((n/2-1,depth+sep,n-1,sep),width=5,fill='rgba(255,255,255,255)')
+arrow.save('arrow.png')
