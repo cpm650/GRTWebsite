96b82fdc6a4c6c645e440f4a5456150b4854f4a1
“KueTone” <lujohnathon@gmail.com>
2021-03-28 22:19:08 -0700

    changed destination of updates

--- updates.php
+++ updates.php
@@ -1,7 +1,7 @@
 <?php
 include "PHP_Engines/grtXmlEngine.php";
 
-$GLOBALS['pageSource'] = "mentors"; //name of source page, no extensions. Saves a function call in engine
+$GLOBALS['pageSource'] = "updates"; //name of source page, no extensions. Saves a function call in engine
 
 $pageContent = new PageContent();
 $navBar = new navBar();
